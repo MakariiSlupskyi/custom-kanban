bb918b14701979e13dba5f4b2f7657b26cd1b6f8
Makarii Slupskyi <makariislupskyi@gmail.com>
2024-08-26 19:16:53 +0300

    implemented custom kanban

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { CustomKanban } from './components/CustomKanban'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <CustomKanban />
+    </div>
+  )
+}
+
+export default App
